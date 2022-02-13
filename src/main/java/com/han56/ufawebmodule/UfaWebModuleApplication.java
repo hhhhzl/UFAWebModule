fadae207fca7f056223631b3163fe9526d28f709
Han56 <2279627931@qq.com>
2022-02-13 15:09:08 +0800

    项目初始化

---
+++ src/main/java/com/han56/ufawebmodule/UfaWebModuleApplication.java
@@ -0,0 +1,13 @@
+package com.han56.ufawebmodule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UfaWebModuleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UfaWebModuleApplication.class, args);
+    }
+
+}
