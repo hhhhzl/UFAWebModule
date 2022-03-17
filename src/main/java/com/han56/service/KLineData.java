c80407ca3ef70a508fb36503be79182ae2cad90c
Han56 <2279627931@qq.com>
2022-03-17 20:29:44 +0800

    定义KLine接口

---
+++ src/main/java/com/han56/service/KLineData.java
@@ -0,0 +1,19 @@
+package com.han56.service;
+
+/**
+ * @author han56
+ * @description 功能描述：K线
+ * @create 2022/3/16 下午1:52
+ */
+public interface KLineData {
+
+    //获取日K线
+
+
+    //获取周K线
+
+    //获取月K线
+
+    //获取分时线
+
+}
