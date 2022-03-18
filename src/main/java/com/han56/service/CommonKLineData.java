5b0854264d3f3bf2d8df70240ed0a1957bf6d2ad
Han56 <2279627931@qq.com>
2022-03-18 16:31:23 +0800

    定义普通K线数据接口

--- src/main/java/com/han56/service/CommonKLineData.java
+++ src/main/java/com/han56/service/CommonKLineData.java
@@ -1,25 +1,27 @@
 package com.han56.service;
 
+import java.io.IOException;
+
 /**
  * @author han56
  * @description 功能描述：普通K线接口
  * 定义 日 周 月 年K线数据的获取
  * @create 2022/3/16 下午1:52
  */
-public interface KLineData {
+public interface CommonKLineData {
 
     /*
     * 获取日K线
     * 考虑到的情况：
     * 1.输入日期如果是星期六星期天，则返回空并附加提示信息
     * */
-    String getDayKLine(String dayDate);
+    String getDayKLine(String startDate,String endDate,String stockCode) throws IOException;
 
     //获取周K线
-    String getWeekKLine(String weekDate);
+    String getWeekKLine(String startDate,String endDate,String stockCode);
 
     //获取月K线
-    String getMonthKLine(String monthDate);
+    String getMonthKLine(String startDate,String endDate,String stockCode);
 
     //获取季度K线
     String getQuatKLine(String QuatDate);
