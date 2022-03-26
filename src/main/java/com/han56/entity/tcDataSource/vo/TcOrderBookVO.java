5549dd916d59bb80b7c7d38f16c7d5e94665b73e
Han56 <2279627931@qq.com>
2022-03-26 11:10:37 +0800

    封装摆盘数据VO层

---
+++ src/main/java/com/han56/entity/tcDataSource/vo/TcOrderBookVO.java
@@ -0,0 +1,244 @@
+package com.han56.entity.tcDataSource.vo;
+
+import java.io.Serializable;
+import java.util.List;
+
+/**
+ * @author han56
+ * @description 功能描述
+ * @create 2022/3/25 下午7:16
+ */
+public class TcOrderBookVO implements Serializable {
+
+    //买一
+    private String bidOne;
+
+    //买一量
+    private String bidOneVolumn;
+
+    //买二
+    private String bidTwo;
+
+    //买二量
+    private String bidTwoVolumn;
+
+    //买三
+    private String bidThree;
+
+    //买三量
+    private String bidThreeVolumn;
+
+    //买四
+    private String bidFour;
+
+    //买四量
+    private String bidFourVolumn;
+
+    //买五
+    private String bidFive;
+
+    //买五量
+    private String bidFiveVolumn;
+
+    //卖一
+    private String askOne;
+
+    //卖一量
+    private String askOneVolumn;
+
+    //卖二
+    private String askTwo;
+
+    //卖二量
+    private String askTwoVolumn;
+
+    //卖三
+    private String askThree;
+
+    //卖三量
+    private String askThreeVolumn;
+
+    //卖四
+    private String askFour;
+
+    //卖四量
+    private String askFourVolumn;
+
+    //卖五
+    private String askFive;
+
+    //卖五量
+    private String askFiveVolumn;
+
+    //时间戳
+    private Long timestamp;
+
+    public String getBidOne() {
+        return bidOne;
+    }
+
+    public void setBidOne(String bidOne) {
+        this.bidOne = bidOne;
+    }
+
+    public String getBidOneVolumn() {
+        return bidOneVolumn;
+    }
+
+    public void setBidOneVolumn(String bidOneVolumn) {
+        this.bidOneVolumn = bidOneVolumn;
+    }
+
+    public String getBidTwo() {
+        return bidTwo;
+    }
+
+    public void setBidTwo(String bidTwo) {
+        this.bidTwo = bidTwo;
+    }
+
+    public String getBidTwoVolumn() {
+        return bidTwoVolumn;
+    }
+
+    public void setBidTwoVolumn(String bidTwoVolumn) {
+        this.bidTwoVolumn = bidTwoVolumn;
+    }
+
+    public String getBidThree() {
+        return bidThree;
+    }
+
+    public void setBidThree(String bidThree) {
+        this.bidThree = bidThree;
+    }
+
+    public String getBidThreeVolumn() {
+        return bidThreeVolumn;
+    }
+
+    public void setBidThreeVolumn(String bidThreeVolumn) {
+        this.bidThreeVolumn = bidThreeVolumn;
+    }
+
+    public String getBidFour() {
+        return bidFour;
+    }
+
+    public void setBidFour(String bidFour) {
+        this.bidFour = bidFour;
+    }
+
+    public String getBidFourVolumn() {
+        return bidFourVolumn;
+    }
+
+    public void setBidFourVolumn(String bidFourVolumn) {
+        this.bidFourVolumn = bidFourVolumn;
+    }
+
+    public String getBidFive() {
+        return bidFive;
+    }
+
+    public void setBidFive(String bidFive) {
+        this.bidFive = bidFive;
+    }
+
+    public String getBidFiveVolumn() {
+        return bidFiveVolumn;
+    }
+
+    public void setBidFiveVolumn(String bidFiveVolumn) {
+        this.bidFiveVolumn = bidFiveVolumn;
+    }
+
+    public String getAskOne() {
+        return askOne;
+    }
+
+    public void setAskOne(String askOne) {
+        this.askOne = askOne;
+    }
+
+    public String getAskOneVolumn() {
+        return askOneVolumn;
+    }
+
+    public void setAskOneVolumn(String askOneVolumn) {
+        this.askOneVolumn = askOneVolumn;
+    }
+
+    public String getAskTwo() {
+        return askTwo;
+    }
+
+    public void setAskTwo(String askTwo) {
+        this.askTwo = askTwo;
+    }
+
+    public String getAskTwoVolumn() {
+        return askTwoVolumn;
+    }
+
+    public void setAskTwoVolumn(String askTwoVolumn) {
+        this.askTwoVolumn = askTwoVolumn;
+    }
+
+    public String getAskThree() {
+        return askThree;
+    }
+
+    public void setAskThree(String askThree) {
+        this.askThree = askThree;
+    }
+
+    public String getAskThreeVolumn() {
+        return askThreeVolumn;
+    }
+
+    public void setAskThreeVolumn(String askThreeVolumn) {
+        this.askThreeVolumn = askThreeVolumn;
+    }
+
+    public String getAskFour() {
+        return askFour;
+    }
+
+    public void setAskFour(String askFour) {
+        this.askFour = askFour;
+    }
+
+    public String getAskFourVolumn() {
+        return askFourVolumn;
+    }
+
+    public void setAskFourVolumn(String askFourVolumn) {
+        this.askFourVolumn = askFourVolumn;
+    }
+
+    public String getAskFive() {
+        return askFive;
+    }
+
+    public void setAskFive(String askFive) {
+        this.askFive = askFive;
+    }
+
+    public String getAskFiveVolumn() {
+        return askFiveVolumn;
+    }
+
+    public void setAskFiveVolumn(String askFiveVolumn) {
+        this.askFiveVolumn = askFiveVolumn;
+    }
+
+    public Long getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(Long timestamp) {
+        this.timestamp = timestamp;
+    }
+
+}
