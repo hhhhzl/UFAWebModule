5e0e6d177891f35cc94ae7c7f9994c1e74c7907f
Han56 <2279627931@qq.com>
2022-03-26 11:10:06 +0800

    修改适配摆盘最终数据格式

--- src/main/java/com/han56/service/subscribe/SubScribe.java
+++ src/main/java/com/han56/service/subscribe/SubScribe.java
@@ -2,14 +2,18 @@ package com.han56.service.subscribe;
 
 import com.alibaba.fastjson.JSON;
 import com.han56.controller.OrDerBookWsController;
-import com.han56.entity.orderBookBean.OrderBookS2C;
 import com.han56.entity.tcDataSource.TcOrderBookBean;
+import com.han56.entity.tcDataSource.vo.TcOrderBookVO;
+import com.han56.utils.Utils;
 import lombok.extern.slf4j.Slf4j;
 import org.redisson.api.RTopic;
 import org.redisson.api.RedissonClient;
 import org.redisson.api.listener.MessageListener;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+import java.util.List;
 
 /**
  * @author han56
@@ -23,6 +27,9 @@ public class SubScribe {
     @Autowired
     private RedissonClient redissonClient;
 
+    @Autowired
+    private Utils utils;
+
     /*
     * @Param
     * 1.marketCode:String 市场代码 21沪市 22深股
@@ -32,12 +39,21 @@ public class SubScribe {
         String orderParm = "orderbook"+"-"+marketCode+"-"+stockCode;
         RTopic rTopic = redissonClient.getTopic(orderParm);
         // 接收订阅的消息
-        rTopic.addListener(TcOrderBookBean.class, new MessageListener<TcOrderBookBean>() {
+        rTopic.addListener(TcOrderBookVO.class, new MessageListener<TcOrderBookVO>() {
             // 接受订阅的消息
             @Override
-            public void onMessage(CharSequence charSequence, TcOrderBookBean tcOrderBookBean) {
+            public void onMessage(CharSequence charSequence, TcOrderBookVO tcOrderBookVO) {
                 //log.info("接受到消息主题={}，内容={}",charSequence,orderBookS2C.toString());
-                OrDerBookWsController.broadCast( JSON.toJSONString(tcOrderBookBean),orderParm);
+                //对摆盘数据做最后封装
+                HashMap<String,Object> resMap = new HashMap<>();
+                resMap.put("code","200");resMap.put("errMsg","");
+                List<List<String>> asks = utils.setAsksList(tcOrderBookVO);
+                List<List<String>> bids = utils.setBidsList(tcOrderBookVO);
+                HashMap<String,Object> dataMap = new HashMap<>();
+                dataMap.put("asks",asks);dataMap.put("bids",bids);
+                dataMap.put("ts",tcOrderBookVO.getTimestamp());
+                resMap.put("data",dataMap);
+                OrDerBookWsController.broadCast( JSON.toJSONString(resMap),orderParm);
             }
         });
     }
